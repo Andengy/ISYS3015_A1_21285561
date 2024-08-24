d2f16d2b82bdb81854c8e1ee3a277a0b156bbb14
Andengy <andrewnguyen2283@gmail.com>
2024-08-24 21:30:41 +0800

    testing 1

--- src/cat.py
+++ src/cat.py
@@ -1,8 +1,19 @@
 from pet import Pet
 
 class Cat(Pet):
-    def __init__(self, name, age, breed):
-        # TODO: Initialize the dog
-        pass
+    def __init__(self, id, name, breed, age, description, adopted=False):
+        super().__init__(name, breed, age, description)
+        self.id = id
+        self.adopted = adopted
 
-    # TODO: Implement dog-specific methods
+    def get_id(self):
+        return self.id
+
+    def is_adopted(self):
+        return self.adopted
+
+    def set_adopted(self, adopted):
+        self.adopted = adopted
+
+    def __str__(self):
+        return f"{self.get_name()} (Cat, {self.get_breed()}, {self.get_age()} years old, ID: {self.get_id()}, Adopted: {'Yes' if self.is_adopted() else 'No'})"
