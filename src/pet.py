94ad9875078b31ba26e0e2fd4b230b4dd85e5a19
Andengy <andrewnguyen2283@gmail.com>
2024-08-24 21:39:02 +0800

    Added pet

--- src/pet.py
+++ src/pet.py
@@ -1,6 +1,38 @@
-class Pet:
-    def __init__(self, name, age, breed, species):
-        # TODO: Initialize the pet attributes
-        pass
+# src/pet.py
 
-    # TODO: Implement other necessary methods
+class Pet:
+    def __init__(self, name, breed, age, description):
+        self._name = name
+        self._breed = breed
+        self._age = age
+        self._description = description
+
+    def get_name(self):
+        return self._name
+
+    def get_breed(self):
+        return self._breed
+
+    def get_age(self):
+        return self._age
+
+    def get_description(self):
+        return self._description
+
+    def set_name(self, name):
+        self._name = name
+
+    def set_breed(self, breed):
+        self._breed = breed
+
+    def set_age(self, age):
+        self._age = age
+
+    def set_description(self, description):
+        self._description = description
+
+    def __str__(self):
+        return f"{self._name} ({self._breed}, {self._age} years old)"
+
+    def detailed_info(self):
+        return f"Name: {self._name}\nBreed: {self._breed}\nAge: {self._age} years\nDescription: {self._description}"
